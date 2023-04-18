c196fccfa4f06960e7718edd7e6f1a7d97e8d008
ohyajk <ohyajk@gmail.com>
2023-04-18 10:47:32 +0530

    Add Food Controller

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,52 @@
+class FoodsController < ApplicationController
+  before_action :set_food, only: %i[show edit update destroy]
+
+  # GET /foods or /foods.json
+  def index
+    @foods = Food.all
+  end
+
+  # GET /foods/1 or /foods/1.json
+  def show; end
+
+  # GET /foods/new
+  def new
+    @food = Food.new
+  end
+
+  # POST /foods or /foods.json
+  def create
+    @food = current_user.foods.build(food_params)
+
+    if @food.save
+      redirect_to foods_url, notice: 'Food was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /foods/1 or /foods/1.json
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+
+    if @food.destroy
+      flash[:notice] = 'food deleted successfully.'
+    else
+      flash[:error] = 'Failed to delete food.'
+    end
+    redirect_to foods_path
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+  end
+end
