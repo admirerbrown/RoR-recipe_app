b7c8aef1dce7747b8c3d41da7d5faa48f1f56deb
admirerbrown <admirerbrown@gmail.com>
2023-06-08 22:50:14 +0000

    fix confirmation issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_30_212700) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_215023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,10 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_30_212700) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: "Johnny Doey")
+
+Recipe.create(
+  name: "Classic Spaghetti Bolognese",
+  preparation_time: "15 minutes",
+  cooking_time: "1 hour",
+  description: "A delicious and hearty pasta dish with rich tomato sauce and ground beef.",
+  user_id: 1
+)
+
+# Create Recipe 2
+Recipe.create(
+  name: "Lemon Garlic Roasted Chicken",
+  preparation_time: "10 minutes",
+  cooking_time: "45 minutes",
+  description: "Juicy and flavorful roasted chicken with a tangy lemon and garlic marinade.",
+  user_id: 1
+  public: true,
+)
+
+# Create Recipe 3
+Recipe.create(
+  name: "Vegetable Stir-Fry",
+  preparation_time: "20 minutes",
+  cooking_time: "15 minutes",
+  description: "A quick and healthy stir-fry packed with colorful vegetables and savory sauce.",
+  user_id: 1
+  public: true,
+)
+
+# Create Recipe 4
+Recipe.create(
+  name: "Chocolate Chip Cookies",
+  preparation_time: "15 minutes",
+  cooking_time: "12 minutes",
+  description: "Classic homemade chocolate chip cookies with a soft and chewy texture.",
+  user_id: 1
+  public: true,
+)
